976a2f7e815e3cf84a3daa25a1c3ba650d2db316
OSC-ENT-MOSADDIK <mosaddik.hafiz@oscillosoft.com.au>
2021-03-03 16:11:31 +0600

    face book

---
+++ read.py
@@ -0,0 +1,168 @@
+from tkinter import *
+from PIL import ImageTk, Image
+import cv2
+
+from tensorflow.keras.applications.mobilenet_v2 import preprocess_input
+from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.models import load_model
+from imutils.video import VideoStream
+import numpy as np
+import imutils
+import time
+import os
+
+def detect_and_predict_mask(frame, faceNet, maskNet):
+    # grab the dimensions of the frame and then construct a blob
+    # from it
+    (h, w) = frame.shape[:2]
+    blob = cv2.dnn.blobFromImage(frame, 1.0, (224, 224),
+        (104.0, 177.0, 123.0))
+
+    # pass the blob through the network and obtain the face detections
+    faceNet.setInput(blob)
+    detections = faceNet.forward()
+    print(detections.shape)
+
+    # initialize our list of faces, their corresponding locations,
+    # and the list of predictions from our face mask network
+    faces = []
+    locs = []
+    preds = []
+
+    # loop over the detections
+    for i in range(0, detections.shape[2]):
+        # extract the confidence (i.e., probability) associated with
+        # the detection
+        confidence = detections[0, 0, i, 2]
+
+        # filter out weak detections by ensuring the confidence is
+        # greater than the minimum confidence
+        if confidence > 0.5:
+            # compute the (x, y)-coordinates of the bounding box for
+            # the object
+            box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+            (startX, startY, endX, endY) = box.astype("int")
+
+            # ensure the bounding boxes fall within the dimensions of
+            # the frame
+            (startX, startY) = (max(0, startX), max(0, startY))
+            (endX, endY) = (min(w - 1, endX), min(h - 1, endY))
+
+            # extract the face ROI, convert it from BGR to RGB channel
+            # ordering, resize it to 224x224, and preprocess it
+            face = frame[startY:endY, startX:endX]
+            face = cv2.cvtColor(face, cv2.COLOR_BGR2RGB)
+            face = cv2.resize(face, (224, 224))
+            face = img_to_array(face)
+            face = preprocess_input(face)
+
+            # add the face and bounding boxes to their respective
+            # lists
+            faces.append(face)
+            locs.append((startX, startY, endX, endY))
+
+    # only make a predictions if at least one face was detected
+    if len(faces) > 0:
+        # for faster inference we'll make batch predictions on *all*
+        # faces at the same time rather than one-by-one predictions
+        # in the above `for` loop
+        faces = np.array(faces, dtype="float32")
+        preds = maskNet.predict(faces, batch_size=32)
+
+    # return a 2-tuple of the face locations and their corresponding
+    # locations
+    return (locs, preds)
+
+
+# load our serialized face detector model from disk
+prototxtPath = r"face_detector\deploy.prototxt"
+weightsPath = r"face_detector\res10_300x300_ssd_iter_140000.caffemodel"
+faceNet = cv2.dnn.readNet(prototxtPath, weightsPath)
+
+# load the face mask detector model from disk
+maskNet = load_model("mask_detector.model")
+
+
+root = Tk()
+root.geometry("1200x480")
+
+leftsideFrame = Frame(root)
+welcomeFrame = Frame(root)
+coronaMessage = Frame(root)
+vedeioSteamFrame = Frame(root)
+
+leftsideFrame.place(x=0, y=100, anchor="nw", width=385, height=300)
+welcomeFrame.place(x=0, y=0, anchor="nw", width=800, height=30)
+coronaMessage.place(x=0 , y= 300, anchor="nw", width=450, height=30)
+vedeioSteamFrame.place(x=500 , y= 50, anchor="nw", width=800, height=400)
+
+
+
+# Add WelCome Level 
+w = Label (welcomeFrame,font='Helvetica 18 bold', text= "Welcome to coronavirus detection application!")
+w.pack()
+
+
+# Add Data Page
+w = Label(coronaMessage,font='Helvetica 20 bold', fg="green",text= "Mask Detected")
+w.pack()
+
+
+w = Label(leftsideFrame,font='Helvetica 14 bold',text= "NO OF PEOPLE ENTRY TODAY: 10")
+w.pack()
+
+
+#edeioSteamFrame.grid()
+
+# Create a label in the frame
+lmain = Label(vedeioSteamFrame)
+lmain.grid()
+
+# Capture from camera
+cap = cv2.VideoCapture(0)
+# function for video streaming
+def video_stream():
+    _, frame = cap.read()
+    frame = imutils.resize(frame, width=400)
+
+    # detect faces in the frame and determine if they are wearing a
+	# face mask or not
+    (locs, preds) = detect_and_predict_mask(frame, faceNet, maskNet)
+
+
+    # loop over the detected face locations and their corresponding
+    # locations
+    for (box, pred) in zip(locs, preds):
+        # unpack the bounding box and predictions
+        (startX, startY, endX, endY) = box
+        (mask, withoutMask) = pred
+
+        # determine the class label and color we'll use to draw
+        # the bounding box and text
+        label = "Mask" if mask > withoutMask else "No Mask"
+        color = (0, 255, 0) if label == "Mask" else (0, 0, 255)
+
+        # include the probability in the label
+        label = "{}: {:.2f}%".format(label, max(mask, withoutMask) * 100)
+
+        # display the label and bounding box rectangle on the output
+        # frame
+        cv2.putText(frame, label, (startX, startY - 10),
+            cv2.FONT_HERSHEY_SIMPLEX, 0.45, color, 2)
+        cv2.rectangle(frame, (startX, startY), (endX, endY), color, 2)
+
+    # show the output frame
+    #cv2.imshow("Frame", frame)
+    #key = cv2.waitKey(1) & 0xFF
+
+   
+
+    cv2image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGBA)
+    img = Image.fromarray(cv2image)
+    imgtk = ImageTk.PhotoImage(image=img)
+    lmain.imgtk = imgtk
+    lmain.configure(image=imgtk)
+    lmain.after(1, video_stream) 
+
+video_stream()
+root.mainloop()
